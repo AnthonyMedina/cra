0d61ed4d9c1566e40ce287f381b762ee0623d087
Anthony Medina <antmedina91@gmail.com>
2019-04-20 23:02:04 +0100

    update app templates to not use implicit return

--- templates/typescript/src/app.tsx
+++ templates/typescript/src/app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './app.css';
 
-const App = () => (
-  <div className="app">
-    <h1>CRA</h1>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="app">
+      <h1>CRA</h1>
+    </div>
+  );
+};
 
 export default App;
